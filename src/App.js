a1d9221e0a3bb94337a977ba2ab6a7b93352567d
unknown <tearsoffair@gmail.com>
2020-11-25 19:49:50 +0600

    new functionaly added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import React,{useEffect, useState} from 'react';
 import './App.css';
 
 const App =()=>{
+
+  const [title,setTitle]=useState('Chittagong');
+
+  const onClickHandler = () => {
+    setTitle('Kaptai');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Hello Chittagong</h2>
-        <p>Ambagan Chittagong</p>
+        <h2>Hello {title}</h2>
+        <p>{title} Chittagong</p>
+        <button onClick={onClickHandler}>Click and Watch</button>
       </header>
     </div>
   );
